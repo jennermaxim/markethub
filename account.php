9b3dc63676052ddf2b386207c183d7f6b49fef21
JennerMaxim <jennersi1remaxim@gmail.com>
2024-03-08 20:16:14 +0300

    Setting Up the environment

--- account.php
+++ account.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MarKetHub</title>
+    <title>Accout</title>
 </head>
 <body>
-    <h1>Home page</h1>
+    <h1>Account Page</h1>
 </body>
 </html>
