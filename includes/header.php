e36f3fe044497e72b3179201d1ce5bd4b7529b45
jennermaxim <jennersi1remaxim@gmail.com>
2024-04-07 12:57:43 +0300

    Add the Header

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/styles/header.css">
 </head>
 
 <body>
+    <div class="header">
+        <div class="navbar">
+            <div class="logo"><h1>SALONHUB</h1></div>
+            <div class="right-nav">
+                <a href="account.php">Do you own a salon?</a>
+            </div>
+        </div>
+    </div>
