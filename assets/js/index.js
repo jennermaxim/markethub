1f9302740a3d18cdc56d2f4550c9cf985bf5bc6a
jennermaxim <jennersi1remaxim@gmail.com>
2024-04-09 19:57:25 +0300

    Finish the The Pup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,14 @@
 let send = document.querySelector(".send");
 let close = document.querySelector(".close");
 let model = document.querySelector(".model");
-close.onclick = () =>{
-    alert("Hi");
-}
 
 send.onclick = () =>{
-
+    model.classList.add("open");
 }
+close.onclick = () =>{
+    model.classList.remove("open");
+}
+
+// model.onclick = () => {
+//     model.style.display = 'none';
+// }
