529ced478735ac0efa8ba0ad33de167b2a33f169
jennermaxim <jennersi1remaxim@gmail.com>
2024-04-10 00:23:57 +0300

    Add the Send Email Feature

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,28 @@ let send = document.querySelector(".send");
 let close = document.querySelector(".close");
 let model = document.querySelector(".model");
 
-send.onclick = () =>{
-    model.classList.add("open");
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('service_sh26x5v', 'template_x7928zo', this)
+            .then(() => {
+                // console.log('SUCCESS!');
+                model.classList.add("open");
+                this.reset();
+                setTimeout(() => {
+                    model.classList.remove("open");
+                }, 15000);
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
 }
-close.onclick = () =>{
+
+// send.onclick = () =>{
+//     model.classList.add("open");
+// }
+close.onclick = () => {
     model.classList.remove("open");
 }
 
--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php include 'includes/header.php'; ?>
-<button class="send">Send</button>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
+
+<script type="text/javascript">
+    (function () {
+        emailjs.init({
+            publicKey: "7Ptm1YZz0bFzUGfec",
+        });
+    })();
+</script>
+
+<div class="contact">
+    <form id="contact-form">
+        <input type="hidden" name="contact_number" value="697483">
+        <input type="hidden" name="to_name" value="SalonHub">
+        <input type="text" name="user_name" placeholder="Full Name" required>
+        <input type="email" name="user_email" placeholder="Email">
+        <input type="tel" name="user_contact" placeholder="Phone Number" required>
+        <input type="text" name="user_address" placeholder="Address" required>
+        <select name="service" id="" required>
+            <option value="">Select a Services</option>
+            <option value="Hair Cut">Hair Cut</option>
+            <option value="Tressing">Tressing</option>
+        </select>
+        <input type="date" name="date" required>
+        <textarea name="message" rows="5" placeholder="Message"></textarea>
+        <input class="send" type="submit" value="Send">
+    </form>
+</div>
+
 <div class="model">
     <div class="content">
         <h2 class="title">Thank you for Contacting SalonHub</h2>
